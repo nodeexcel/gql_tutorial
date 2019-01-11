2127d8331f017806066422d2f67a1322be4b555f
etech <email@gmail.com>
2019-01-11 15:34:08 +0530

    tutorial

--- index.js
+++ index.js
@@ -2,6 +2,46 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var graphql = require('graphql');
 
+
+const users = [
+    {
+        id: 1,
+        name : "manish"
+    },
+    {
+        id: 2,
+        name : "test"
+    },
+    {
+        id: 3,
+        name : "test2"
+    }
+]
+
+const address = [
+    {
+        id: 1,
+        address : "test",
+        country: "IN",
+        phone: "000",
+        user_id: 1
+    },
+    {
+        id: 2,
+        address : "test2",
+        country: "IN",
+        phone: "2222",
+        user_id: 1
+    },
+    {
+        id: 3,
+        address : "test3",
+        country: "IN",
+        phone: "3333",
+        user_id: 2
+    }
+]
+
 const countryEnumType = new graphql.GraphQLEnumType({
     name: 'countryEnum',
     values: {
